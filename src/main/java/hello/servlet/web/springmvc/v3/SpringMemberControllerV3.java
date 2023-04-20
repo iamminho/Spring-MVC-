d2f971391a57ea3fc682d1e87383bf9dc9ef448c
iamminho <minho100227@kakao.com>
2023-04-20 18:08:26 +0900

    스프링MVC - 실용적인 방식

---
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,48 @@
+package hello.servlet.web.springmvc.v3;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    // @RequestMapping(value = "/new-form", method = RequestMethod.GET) //RequestMethod가 GET인 경우에만 호출해준다.
+    @GetMapping("/new-form") //위에꺼 줄인거
+    public String newForm() {
+        return "new-form";
+    }
+
+    //@RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save") //위에꺼 줄인거
+    public String save(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age,
+            Model model) {
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.addAttribute("member", member);
+        return "save-result";
+    }
+
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping()
+    public String members(Model model) {
+
+        List<Member> members = memberRepository.findAll();
+
+        model.addAttribute("members", members);
+        return "members";
+    }
+}
